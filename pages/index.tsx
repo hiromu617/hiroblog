f3f28b7f34e52f9456dc1a6c0d779e523849ddb2
hiro <hiromhiro617@gmail.com>
2022-03-12 22:15:02 +0900

    :heavy_plus_sign: add prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <div>
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
-<button className="btn">Button</button>
+      <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
+      <button className="btn">Button</button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}",
-  "./srcs/**/*.{js,ts,jsx,tsx}",],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './srcs/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("daisyui")
-  ],
-}
+  plugins: [require('daisyui')],
+};
