84228781b23d9371c98797ae419ffe934f00b989
hiro <hiromhiro617@gmail.com>
2022-03-12 21:36:04 +0900

    :heavy_plus_sign: add daisyuui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,12 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
+    <div>
     <h1 className="text-3xl font-bold underline text-red-600">
       Hello world!
     </h1>
+<button className="btn">Button</button>
+    </div>
   )
 }
 
