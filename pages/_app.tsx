ea736fe2e7f3e690e16477769c18afef1bbf7f61
hiro <hiromhiro617@gmail.com>
2022-03-13 22:55:21 +0900

    :sparkles: add Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 import { Header } from '../src/components/Layout/Header';
+import { Footer } from '../src/components/Layout/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,17 +1,25 @@
 import { VFC } from 'react';
 import Link from 'next/link';
+import { FaGithub } from 'react-icons/fa';
 
 export const Header: VFC = () => (
-  <div className="navbar bg-base-100">
+  <div className="navbar bg-base-200">
     <div className="flex-1">
       <Link href="/">
-        <a className="btn btn-ghost normal-case text-5xl">🛹</a>
+        <a className="btn btn-ghost normal-case text-4xl">🛹</a>
       </Link>
     </div>
     <div className="flex-none">
       <ul>
         <li>
-          <a></a>
+          <a
+            href="https://github.com/hiromu617"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="btn btn-circle btn-ghost"
+          >
+            <FaGithub size={36} />
+          </a>
         </li>
       </ul>
     </div>
