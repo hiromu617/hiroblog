cd46ac002f0e8dd1b2c2687c6ce6c7e8c2a59dd7
hiro <hiromhiro617@gmail.com>
2022-03-20 22:16:53 +0900

    :sparkles: add page file

---
+++ pages/[tag]/page/[id].tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from 'next';
+import { client } from '../../../src/libs/client';
+import type { Blog, BlogRes } from '../../../src/features/Blog/types';
+import { BlogDetail } from '../../../src/features/Blog/components/BlogDetail';
+import { ShareButtons } from '../../../src/components/Elements/ShareButtons';
+
+type Props = {
+  blog: Blog;
+};
+
+const BlogId: NextPage<Props> = ({ blog }) => {
+  return (
+    <div className="w-full md:w-3/5 justify-center m-auto">
+      {/* <div className="mb-10">
+        <BlogDetail blog={blog} />
+      </div>
+      <div className="mb-10">
+        <ShareButtons blog={blog} />
+      </div> */}
+    </div>
+  );
+};
+
+export default BlogId;
+
+// export const getStaticPaths = async () => {
+//   const data: BlogRes = await client.get({ endpoint: 'blogs' });
+//   const paths = data.contents.map((content) => `/blogs/${content.id}`);
+
+//   return { paths, fallback: false };
+// };
+
+// export const getStaticProps = async (context: any) => {
+//   const id = context.params.id;
+//   const data: Blog = await client.get({ endpoint: 'blogs', contentId: id });
+//   return {
+//     props: {
+//       blog: data,
+//     },
+//   };
+// };
