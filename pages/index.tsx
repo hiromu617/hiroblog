af00ba374b7b177c43e02eea6e58d59f1cdd3a04
hiro <hiromhiro617@gmail.com>
2022-03-20 18:42:38 +0900

    :sparkles: add pagination component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import type { Blog, BlogRes } from '../src/features/Blog/types';
 import { client } from '../src/libs/client';
 import { BlogCard } from '../src/features/Blog/components/BlogCard';
+import { Pagination } from '../src/components/Elements/Pagination';
 
 type Props = {
   blogs: Blog[];
@@ -10,11 +11,14 @@ type Props = {
 const Home: NextPage<Props> = ({ blogs }) => {
   return (
     <div className="w-full md:w-3/5 justify-center m-auto">
-      <ul className="flex flex-col gap-5 w-full">
+      <ul className="flex flex-col gap-5 w-full mb-10">
         {blogs.map((blog, i) => (
           <BlogCard blog={blog} key={blog.id} isNew={i === 0} />
         ))}
       </ul>
+      <div>
+        <Pagination totalCount={2}/>
+      </div>
     </div>
   );
 };
---
+++ src/components/Elements/Pagination.tsx
@@ -0,0 +1,28 @@
+import { VFC } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+type Props = {
+  totalCount: number;
+};
+
+const PER_PAGE = 12;
+const range = (start: number, end: number) => [...Array(end - start + 1)].map((_, i) => start + i);
+
+export const Pagination: VFC<Props> = ({ totalCount }) => {
+  const router = useRouter();
+  const { id } = router.query;
+  const currentPage: number = id ? +id : 1;
+
+  return (
+    <div className="btn-group justify-center drop-shadow-xl">
+      {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
+        <Link href={`/blogs/page/${number}`} key={index} passHref>
+          <button className={`btn hover:btn-primary ${currentPage === number && 'btn-active'}`}>
+            {number}
+          </button>
+        </Link>
+      ))}
+    </div>
+  );
+};
