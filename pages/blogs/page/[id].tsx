78e02065307254871fce95f5ee8e014b788d7d5d
hiro <hiromhiro617@gmail.com>
2022-03-21 13:22:26 +0900

    :pencil2: fix

--- pages/blogs/page/[id].tsx
+++ pages/blogs/page/[id].tsx
@@ -3,23 +3,28 @@ import { client } from '../../../src/libs/client';
 import { BlogCard } from '../../../src/features/Blog/components/BlogCard';
 import { Pagination } from '../../../src/components/Elements/Pagination';
 import { PER_PAGE } from '../../../src/const/index';
-import { Blog } from '../../../src/api/types';
-import { MicroCMSListContent} from 'microcms-js-sdk';
+import { Blog, Tag } from '../../../src/api/types';
+import { MicroCMSListContent } from 'microcms-js-sdk';
+import { TagList } from '../../../src/features/Tag/components/TagList';
 
 type Props = {
   blogs: (Blog & MicroCMSListContent)[];
+  tags: (Tag & MicroCMSListContent)[];
   totalCount: number;
 };
 
-const BlogPageId: NextPage<Props> = ({ blogs, totalCount }) => {
+const BlogPageId: NextPage<Props> = ({ blogs, tags, totalCount }) => {
   return (
     <div className="w-full md:w-3/5 justify-center m-auto">
+      <div className="mb-7">
+        <TagList tags={tags} />
+      </div>
       <ul className="flex flex-col gap-5 w-full mb-10">
         {blogs.map((blog, i) => (
           <BlogCard blog={blog} key={blog.id} isNew={i === 0} />
         ))}
       </ul>
-      <div className='mb-10'>
+      <div className="mb-10">
         <Pagination totalCount={totalCount} />
       </div>
     </div>
@@ -43,11 +48,15 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async (context: any) => {
   const id = context.params.id;
-  const response = await client.blogs.$get({query :{offset: (id - 1) * PER_PAGE, limit: PER_PAGE}} );
+  const response = await client.blogs.$get({
+    query: { offset: (id - 1) * PER_PAGE, limit: PER_PAGE },
+  });
+  const tagResponse = await client.tags.$get();
 
   return {
     props: {
       blogs: response.contents,
+      tags: tagResponse.contents,
       totalCount: response.totalCount,
     },
   };
