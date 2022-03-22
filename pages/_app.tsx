1fb2b18816fe4bf3a5e3b14eed2dce0c7aa352de
hiro <hiromhiro617@gmail.com>
2022-03-22 19:40:02 +0900

    :sparkles: add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,10 @@ import { Header } from '../src/components/Layout/Header';
 import { Footer } from '../src/components/Layout/Footer';
 import initTwitterScriptInner from 'zenn-embed-elements/lib/init-twitter-script-inner';
 import { useEffect } from 'react';
+import { usePageView, GoogleAnalytics } from '../src/libs/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageView();
   // @ts-ignore
   useEffect(() => import('zenn-embed-elements'), []);
   return (
@@ -16,6 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           __html: initTwitterScriptInner,
         }}
       />
+      <GoogleAnalytics />
       <Header />
       <div className="min-h-screen justify-center py-5 px-3 md:px-5 bg-base-200">
         <Component {...pageProps} />
---
+++ src/libs/gtag.tsx
@@ -0,0 +1,87 @@
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect, VFC } from "react";
+import Head from "next/head"
+
+export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || "";
+// IDが取得できない場合を想定する
+export const existsGaId = GA_ID !== "";
+
+// PVを測定する
+export const pageview = (path: string) => {
+  window.gtag("config", GA_ID, {
+    page_path: path,
+  });
+};
+
+// GAイベントを発火させる
+export const event = ({ action, category, label, value = "" }: Event) => {
+  if (!existsGaId) {
+    return;
+  }
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label ? JSON.stringify(label) : "",
+    value,
+  });
+};
+
+// _app.tsx で読み込む
+export const usePageView = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!existsGaId) {
+      return;
+    }
+
+    const handleRouteChange = (path: string) => {
+      pageview(path);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+};
+
+// _app.tsx で読み込む
+export const GoogleAnalytics = () => (
+  <>
+   <Head>
+        {/* Google Analytics */}
+        {existsGaId && (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+              }}
+            />
+          </>
+        )}
+      </Head>
+  </>
+);
+// イベントを型で管理
+type ContactEvent = {
+  action: "submit_form";
+  category: "contact";
+};
+
+type ClickEvent = {
+  action: "click";
+  category: "other";
+};
+
+export type Event = (ContactEvent | ClickEvent) & {
+  label?: Record<string, string | number | boolean>;
+  value?: string;
+};
