50a6de665a33cfb76e88a029a5e20d3b960cf91a
hiro <hiromhiro617@gmail.com>
2022-03-22 17:29:12 +0900

    :sparkles: impl embed content

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,6 @@ module.exports = {
   darkMode: 'media',
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: ['winter', 'night'],
+    themes: ['winter'],
   },
 };
