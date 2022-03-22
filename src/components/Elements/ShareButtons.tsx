e2e80920574c01d7c49ae5dcdb078bb7370e2dc0
hiro <hiromhiro617@gmail.com>
2022-03-22 17:50:55 +0900

    :bug: fix share url

--- src/components/Elements/ShareButtons.tsx
+++ src/components/Elements/ShareButtons.tsx
@@ -4,6 +4,7 @@ import { TwitterShareButton } from 'react-share';
 import { FaTwitter, FaShare } from 'react-icons/fa';
 import { MicroCMSObjectContent } from 'microcms-js-sdk';
 import { useRouter } from 'next/router';
+import { SITE_URL } from '../../const/index';
 
 type Props = {
   blog: Blog & MicroCMSObjectContent;
@@ -18,7 +19,7 @@ export const ShareButtons: VFC<Props> = ({ blog }) => {
         <div className="text-xl font-bold">Share</div>
         <FaShare size={20} />
       </div>
-      <TwitterShareButton title={blog.title} url={`https://hiroblog.vercel.app${router.asPath}`}>
+      <TwitterShareButton title={blog.title} url={`${SITE_URL}/${router.asPath}`}>
         <button className="btn btn-circle hover:btn-primary shadow-xl">
           <FaTwitter size={24} />
         </button>
--- src/const/index.ts
+++ src/const/index.ts
@@ -1,3 +1,4 @@
 const PER_PAGE = 12;
+const SITE_URL = 'https://blog.hiromu617.vercel.app';
 
-export { PER_PAGE };
+export { PER_PAGE, SITE_URL };
