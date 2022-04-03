13d9a36bddc2630264fb26fd85e03db0c87c25bc
hiro <hiromhiro617@gmail.com>
2022-04-03 16:22:17 +0900

    :sparkles: add ogp, meta info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage, GetStaticProps } from 'next';
+import Head from 'next/head';
 import { Blog, Tag } from '../src/api/types';
 import { client } from '../src/libs/client';
 import { BlogCard } from '../src/features/Blog/components/BlogCard';
@@ -7,6 +8,7 @@ import { PER_PAGE } from '../src/const';
 import { Pagination } from '../src/components/Elements/Pagination';
 import { MicroCMSListContent } from 'microcms-js-sdk';
 import { differenceInDays } from 'date-fns';
+import { generateOgpUrl } from '../src/utils/index';
 
 type Props = {
   blogs: (Blog & MicroCMSListContent)[];
@@ -16,23 +18,32 @@ type Props = {
 
 const Home: NextPage<Props> = ({ blogs, tags, totalCount }) => {
   return (
-    <div className="w-full md:w-3/5 justify-center m-auto">
-      <div className="mb-7">
-        <TagList tags={tags} />
+    <>
+      <Head>
+        <title>blog.hiromu617</title>
+        <meta property="description" content="hiromu617のブログです" />
+        <meta property="og:title" content="blog.hiromu617" />
+        <meta property="og:description" content="hiromu617のブログです" />
+        <meta name="twitter:card" content="summary" />
+      </Head>
+      <div className="w-full md:w-3/5 justify-center m-auto">
+        <div className="mb-7">
+          <TagList tags={tags} />
+        </div>
+        <ul className="flex flex-col gap-5 w-full mb-10">
+          {blogs.map((blog, i) => (
+            <BlogCard
+              blog={blog}
+              key={blog.id}
+              isNew={differenceInDays(new Date(), new Date(blog.publishedAt)) <= 7}
+            />
+          ))}
+        </ul>
+        <div className="mb-10">
+          <Pagination totalCount={totalCount} />
+        </div>
       </div>
-      <ul className="flex flex-col gap-5 w-full mb-10">
-        {blogs.map((blog, i) => (
-          <BlogCard
-            blog={blog}
-            key={blog.id}
-            isNew={differenceInDays(new Date(), new Date(blog.publishedAt)) <= 7}
-          />
-        ))}
-      </ul>
-      <div className="mb-10">
-        <Pagination totalCount={totalCount} />
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export const generateOgpUrl = (title: string) =>
+  `https://res.cloudinary.com/dee5jtedf/image/upload/l_text:Sawarabi%20Gothic_50_bold:${title},co_rgb:456593,w_500,c_fit/v1648964616/ogp_wd5psv.png`;
